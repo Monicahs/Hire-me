3e141c83a9319a6862652cf7920f66d8eb9ee589
Monika <monikahsmonu@gmail.com>
2025-05-08 01:08:28 +0530

    Delete unwanted files

--- Hire me/pythonfiles/delete_job.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-def delete_job(job_id):
-    conn = sqlite3.connect('db/hireme.db')
-    cursor = conn.cursor()
-    cursor.execute('DELETE FROM jobs WHERE id = ?', (job_id,))
-    conn.commit()
-    conn.close()
