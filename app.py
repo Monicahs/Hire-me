3b5521fda56e898a0992d3633d850dc5fe2b30cf
Monika HS <121927505+Monicahs@users.noreply.github.com>
2025-05-08 01:28:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -214,5 +214,5 @@ def remove_job(job_id):
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 if __name__ == '__main__':
-    port=int(os.environ.get("PORT",5000))
+    port=int(os.environ.get("PORT",10000))
     app.run(host="0.0.0.0",port=port,debug=True)
